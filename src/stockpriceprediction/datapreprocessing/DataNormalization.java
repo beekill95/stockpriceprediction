6874c1283826bc6ca8f5a7e78670ccad1a0c786a
beekill <nnn_bikiu0707@yahoo.com>
2016-10-20 09:30:34 +0700

    Implement basic data normalization method but havent tested yet

---
+++ src/stockpriceprediction/datapreprocessing/DataNormalization.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stockpriceprediction.datapreprocessing;
+
+import stockpriceprediction.helper.BasicStatistics;
+
+/**
+ *
+ * @author beekill
+ */
+public interface DataNormalization {
+    double[][] normalizeData(double[][] data);
+    double normalizeNewData(int index, double newData);
+    double toOriginalData(int index, double normalizedData);
+}
